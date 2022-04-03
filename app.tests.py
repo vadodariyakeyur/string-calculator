f6a1f5c4122cc5e3d881173d92026232a417e93a
vadodariyakeyur <vadodariyakeyur321@gmail.com>
2022-04-03 13:52:48 +0530

    TDD(Failing): Added test case for single number string

--- app.tests.py
+++ app.tests.py
@@ -7,5 +7,9 @@ class StringCalculatorTests(unittest.TestCase):
         result = string_calculator("")
         self.assertEqual(result, 0)
 
+    def test_string_calculator_should_return_seven(self) -> None:
+        result = string_calculator("7")
+        self.assertEqual(result, 7)
+
 if __name__ == '__main__':
     unittest.main()
